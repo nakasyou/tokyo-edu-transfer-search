bd4635d388c03a155327c932758aa2a79606a69d
nakasyou <nakasyou1103@gmail.com>
2023-03-23 18:16:52 +0900

    async

--- index.js
+++ index.js
@@ -1,67 +1,66 @@
 (async()=>{
   const res=await fetch("https://gist.githubusercontent.com/nakasyou/376d596fee53f0715dbeb6880a516883/raw/851aff6a2298d877442a6fbab2af4fd668ac4135/tokyo-edu-transfer-peoples-2023.json")
-const data=await res.json();
+  const data=await res.json();
 
-const citys=new Set();
-const schools=new Set();
-data.forEach(d=>{
-  schools.add(d.NewPlaceName);
-  schools.add(d.OldPlaceName);
-
-  console.log(d.NewPlaceName.match(/^.+立/)[0])
-});
-const $schools=document.getElementById("schools");
-const $result=document.getElementById("result");
-Array.from(schools).sort().forEach(school=>{
-  const option=document.createElement("option");
-  option.value=school;
-  option.textContent=school;
-  $schools.append(option);
-});
-$schools.addEventListener("change",event=>{
-  $result.innerHTML=`<p id="searching">けんさくちゅう...</p>`;
-  const target=event.target.value;
-  const hey=[];
-  const bye=[];
+  const citys=new Set();
+  const schools=new Set();
   data.forEach(d=>{
-    if(d.OldPlaceName===target)
-      bye.push(d);
-    if(d.NewPlaceName===target)
-      hey.push(d);
-  });
-  const ans=document.createElement("div");
-  ans.style.display="flex";
-  {
-    const heyElement=document.createElement("div");
-    ans.append(heyElement);
-    const heyTitle=document.createElement("h2");
-    heyTitle.textContent="転入";
-    const heyUl=document.createElement("ul");
-    hey.forEach(({Name,OldPlaceName,Reason,NewJobType,OldJobType})=>{
-      const li=document.createElement("li");
-      li.innerHTML=`<b>${Name}</b><br>(To ${OldPlaceName})<br>${Reason}(${OldJobType}->${NewJobType})`;
-      heyUl.append(li);
-    });
-    heyElement.append(heyTitle);
-    heyElement.append(heyUl);
-  }
-  {
-    const byeElement=document.createElement("div");
-    ans.append(byeElement);
-    const byeTitle=document.createElement("h2");
-    byeTitle.textContent="転出";
-    const byeUl=document.createElement("ul");
-    bye.forEach(({Name,NewPlaceName,Reason,NewJobType,OldJobType})=>{
-      const li=document.createElement("li");
-      li.innerHTML=`<b>${Name}</b><br>(From ${NewPlaceName})<br>${Reason}(${OldJobType}->${NewJobType})`;
-      byeUl.append(li);
-    });
-    byeElement.append(byeTitle);
-    byeElement.append(byeUl);
-  }
-  $result.append(ans);
-  document.getElementById("searching").hidden=true;
-});
+    schools.add(d.NewPlaceName);
+    schools.add(d.OldPlaceName);
 
+    console.log(d.NewPlaceName.match(/^.+立/)[0])
+  });
+  const $schools=document.getElementById("schools");
+  const $result=document.getElementById("result");
+  Array.from(schools).sort().forEach(school=>{
+    const option=document.createElement("option");
+    option.value=school;
+    option.textContent=school;
+    $schools.append(option);
+  });
+  $schools.addEventListener("change",event=>{
+    $result.innerHTML=`<p id="searching">けんさくちゅう...</p>`;
+    const target=event.target.value;
+    const hey=[];
+    const bye=[];
+    data.forEach(d=>{
+      if(d.OldPlaceName===target)
+        bye.push(d);
+      if(d.NewPlaceName===target)
+        hey.push(d);
+    });
+    const ans=document.createElement("div");
+    ans.style.display="flex";
+    {
+      const heyElement=document.createElement("div");
+      ans.append(heyElement);
+      const heyTitle=document.createElement("h2");
+      heyTitle.textContent="転入";
+      const heyUl=document.createElement("ul");
+      hey.forEach(({Name,OldPlaceName,Reason,NewJobType,OldJobType})=>{
+        const li=document.createElement("li");
+        li.innerHTML=`<b>${Name}</b><br>(To ${OldPlaceName})<br>${Reason}(${OldJobType}->${NewJobType})`;
+        heyUl.append(li);
+      });
+      heyElement.append(heyTitle);
+      heyElement.append(heyUl);
+    }
+    {
+      const byeElement=document.createElement("div");
+      ans.append(byeElement);
+      const byeTitle=document.createElement("h2");
+      byeTitle.textContent="転出";
+      const byeUl=document.createElement("ul");
+      bye.forEach(({Name,NewPlaceName,Reason,NewJobType,OldJobType})=>{
+        const li=document.createElement("li");
+        li.innerHTML=`<b>${Name}</b><br>(From ${NewPlaceName})<br>${Reason}(${OldJobType}->${NewJobType})`;
+        byeUl.append(li);
+      });
+      byeElement.append(byeTitle);
+      byeElement.append(byeUl);
+    }
+    $result.append(ans);
+    document.getElementById("searching").hidden=true;
+  });
   document.getElementById("loding").hidden=true;
 })
